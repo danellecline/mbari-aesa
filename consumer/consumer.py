5a0a2973c2c6bb7ccb50e42e6913bb6bf3acfceb
Danelle Cline <dcline@mbari.org>
2016-10-06 12:20:23 -0700

    added aws credentials to consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,9 +1,44 @@
 import os, pika, logging, sys, argparse, time
 from argparse import RawTextHelpFormatter
+from botocore.client import ClientError
 from time import sleep
 import boto3
 import botocore
 
+# set up global variables for logging output to STDOUT
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+# check environment variables required for connecting to S3 bucket
+bucket_name = os.environ['S3_BUCKET_NAME']
+
+if os.environ['AWS_ACCESS_KEY_ID'] is None:
+    LOG.warninig('Missing required environment variable: AWS_ACCESS_KEY_ID')
+    sys.exit(-1)
+if os.environ['AWS_SECRET_ACCESS_KEY'] is None:
+    LOG.warninig('Missing required environment variable: AWS_SECRET_ACCESS_KEY')
+    sys.exit(-1)
+if os.environ['AWS_DEFAULT_REGION'] is None:
+    LOG.warninig('Missing required environment variable: AWS_DEFAULT_REGION')
+    sys.exit(-1)
+if bucket_name is None:
+    LOG.warninig('Missing required environment variable: S3_BUCKET_NAME')
+    sys.exit(-1) 
+
+# connect to Amazon S3
+s3 = boto3.resource('s3')
+
+try:
+    s3.meta.client.head_bucket(Bucket=bucket_name)
+except ClientError:
+    LOG.warning("Bucket %s doesn't exist or I don't have access to it " % bucket_name)
+    LOG.warning('Here are all the buckets')
+    for bucket in s3.buckets.all():
+        LOG.warning(bucket.name)
+    sys.exit(-1)
+
+# connect to bucket
+bucket = s3.Bucket(bucket_name)
 
 def on_message(channel, method_frame, header_frame, body):
     print method_frame.delivery_tag
@@ -12,10 +47,15 @@ def on_message(channel, method_frame, header_frame, body):
     LOG.info('Message has been received %s', body)
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
+    # Upload a new file
+    data = open('test.jpg', 'rb')
+    LOG.info('Uploading %s', 'test.jpg')
+    root_dir = body.split('/')[0]
+    bucket.put_object(Key=root_dir + '_results/test.jpg', Body=data)
+    LOG.info('Uploading done')
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    LOG = logging.getLogger(__name__)
     examples = sys.argv[0] + " -p 5672 -s rabbitmq "
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
                                  description='Run consumer.py',
@@ -25,29 +65,6 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--in_folder', action='store', dest='in_folder', help='The bucket_name folder with image tiles to process', required=True)
 
     args = parser.parse_args()
-
-    access_key = os.environ['AWS_ACCESS_KEY_ID']
-    secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    region = os.environ['AWS_DEFAULT_REGION']
-    bucket_name = os.environ['S3_BUCKET_NAME']
-
-    if access_key is None:
-        print "Missing required environment variable: AWS_ACCESS_KEY_ID"
-        sys.exit(-1)
-    if secret_key is None:
-        print "Missing required environment variable: AWS_SECRET_ACCESS_KEY"
-        sys.exit(-1)
-    if region is None:
-        print "Missing required environment variable: AWS_DEFAULT_REGION"
-        sys.exit(-1)
-    if bucket_name is None:
-        print "Missing required environment variable: S3_BUCKET_NAME"
-        sys.exit(-1)
-
-
-    # use Amazon S3
-    s3 = boto3.resource('s3')
-    client = boto3.client('s3')
 
     # sleep a few seconds to allow RabbitMQ server to come up
     sleep(5)
