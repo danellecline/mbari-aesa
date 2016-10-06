9b805dd346301c928e95fb79d561c2c01b9b974a
Danelle Cline <dcline@mbari.org>
2016-10-06 10:10:00 -0700

    added aws credentials to consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,6 +1,9 @@
-import pika, logging, sys, argparse, time
+import os, pika, logging, sys, argparse, time
 from argparse import RawTextHelpFormatter
 from time import sleep
+import boto3
+import botocore
+
 
 def on_message(channel, method_frame, header_frame, body):
     print method_frame.delivery_tag
@@ -11,25 +14,43 @@ def on_message(channel, method_frame, header_frame, body):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    LOG = logging.getLogger(__name__)
     examples = sys.argv[0] + " -p 5672 -s rabbitmq "
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
                                  description='Run consumer.py',
                                  epilog=examples)
-    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.')
-    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.')
+    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.', required=False, default=5672)
+    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.', required=False, default='rabbitmq')
+    parser.add_argument('-i', '--in_folder', action='store', dest='in_folder', help='The bucket_name folder with image tiles to process', required=True)
 
     args = parser.parse_args()
-    if args.port == None:
-        print "Missing required argument: -p/--port"
-        sys.exit(1)
-    if args.server == None:
-        print "Missing required argument: -s/--server"
-        sys.exit(1)
+
+    access_key = os.environ['AWS_ACCESS_KEY_ID']
+    secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    region = os.environ['AWS_DEFAULT_REGION']
+    bucket_name = os.environ['S3_BUCKET_NAME']
+
+    if access_key is None:
+        print "Missing required environment variable: AWS_ACCESS_KEY_ID"
+        sys.exit(-1)
+    if secret_key is None:
+        print "Missing required environment variable: AWS_SECRET_ACCESS_KEY"
+        sys.exit(-1)
+    if region is None:
+        print "Missing required environment variable: AWS_DEFAULT_REGION"
+        sys.exit(-1)
+    if bucket_name is None:
+        print "Missing required environment variable: S3_BUCKET_NAME"
+        sys.exit(-1)
+
+
+    # use Amazon S3
+    s3 = boto3.resource('s3')
+    client = boto3.client('s3')
 
     # sleep a few seconds to allow RabbitMQ server to come up
     sleep(5)
-    logging.basicConfig(level=logging.INFO)
-    LOG = logging.getLogger(__name__)
     credentials = pika.PlainCredentials('guest', 'guest')
     parameters = pika.ConnectionParameters(args.server,
                                            int(args.port),
