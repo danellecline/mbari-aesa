8fa2d84dc2821f8fd3141b3c5992fd6ee115dc79
Danelle Cline <dcline@mbari.org>
2016-10-06 12:20:54 -0700

    added aws credentials to producer and file check

--- producer/producer.py
+++ producer/producer.py
@@ -2,75 +2,71 @@ import os, pika, logging, sys, argparse
 from argparse import RawTextHelpFormatter
 from time import sleep
 import boto3
+import botocore
 from botocore.client import ClientError
 
+def file_exists(bucket_name, file):
+    try:
+        s3.Object(bucket_name, file).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            exists = False
+        else:
+            raise e
+    else:
+        exists = True
+
+    return exists
+
+
 if __name__ == '__main__':
-    examples = sys.argv[0] + " -p 5672 -s rabbitmq -m 'Hello' "
+    logging.basicConfig(level=logging.INFO)
+    LOG = logging.getLogger(__name__)
+
+    examples = sys.argv[0] + " -p 5672 -s rabbitmq -i 'M56_tiles' -o 'M56_tile_detections'"
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
                                  description='Run producer.py',
                                  epilog=examples)
-    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.')
-    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.')
-    parser.add_argument('-m', '--message', action='store', dest='message', help='The message to send', required=False, default='Hello')
+    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.', required=False, default=5672)
+    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.', required=False, default='rabbitmq')
+    parser.add_argument('-i', '--in_folder', action='store', dest='in_folder', help='The bucket_name folder with image tiles to process', required=True)
     parser.add_argument('-r', '--repeat', action='store', dest='repeat', help='Number of times to repeat the message', required=False, default='30')
 
     args = parser.parse_args()
-    if args.port == None:
-        print "Missing required argument: -p/--port"
-        sys.exit(1)
-    if args.server == None:
-        print "Missing required argument: -s/--server"
-        sys.exit(1)
 
-    access_key = os.environ['AWS_ACCESS_KEY_ID']
-    secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    region = os.environ['AWS_DEFAULT_REGION']
+    # check environment variables required for connecting to S3 bucket
+
     bucket_name = os.environ['S3_BUCKET_NAME']
-    bucket_dir = os.environ['S3_BUCKET_DIR_NAME']
-
-    if access_key is None:
-        print "Missing required environment variable: AWS_ACCESS_KEY_ID"
+    if os.environ['AWS_ACCESS_KEY_ID'] is None:
+        LOG.warninig('Missing required environment variable: AWS_ACCESS_KEY_ID')
         sys.exit(-1)
-    if secret_key is None:
-        print "Missing required environment variable: AWS_SECRET_ACCESS_KEY"
+    if os.environ['AWS_SECRET_ACCESS_KEY'] is None:
+        LOG.warninig('Missing required environment variable: AWS_SECRET_ACCESS_KEY')
         sys.exit(-1)
-    if region is None:
-        print "Missing required environment variable: AWS_DEFAULT_REGION"
+    if os.environ['AWS_DEFAULT_REGION'] is None:
+        LOG.warninig('Missing required environment variable: AWS_DEFAULT_REGION')
         sys.exit(-1)
     if bucket_name is None:
-        print "Missing required environment variable: S3_BUCKET_NAME"
-        sys.exit(-1)
-    if bucket_dir is None:
-        print "Missing required environment variable: S3_BUCKET_DIR_NAME"
+        LOG.warninig('Missing required environment variable: S3_BUCKET_NAME')
         sys.exit(-1)
 
-    # use Amazon S3
+    # connect to Amazon S3
     s3 = boto3.resource('s3')
-    client = boto3.client('s3')
 
     try:
         s3.meta.client.head_bucket(Bucket=bucket_name)
     except ClientError:
-        print "Bucket %s doesn't exist or I don't have access to it " % bucket_name
-        print "Here are all the buckets"
+        LOG.warning("Bucket %s doesn't exist or I don't have access to it " % bucket_name)
+        LOG.warning('Here are all the buckets')
         for bucket in s3.buckets.all():
-            print(bucket.name)
+            LOG.warning(bucket.name)
         sys.exit(-1)
 
     bucket = s3.Bucket(bucket_name)
 
-    # create a directory under the main bucket to store the detection results
-    response = client.put_object(
-        Bucket=bucket_name,
-        Body='',
-        Key='detections/'
-        )
-
     # sleep a few seconds to allow RabbitMQ server to come up
     sleep(5)
 
-    logging.basicConfig(level=logging.INFO)
-    LOG = logging.getLogger(__name__)
     credentials = pika.PlainCredentials('guest', 'guest')
     parameters = pika.ConnectionParameters(args.server,
                                            int(args.port),
@@ -84,18 +80,12 @@ if __name__ == '__main__':
     # Turn on delivery confirmations
     channel.confirm_delivery()
 
-    '''for i in range(0, int(args.repeat)):
-        if channel.basic_publish('', q_name, args.message):
-            LOG.info('Message has been delivered')
-        else:
-            LOG.warning('Message NOT delivered')
-
-        sleep(2)'''
-
-    # S3 list all keys with the prefix bucket_dir
-    for obj in bucket.objects.filter(Prefix=bucket_dir + '/'):
-        print('{0}:{1}'.format(bucket.name, obj.key))
-        if '.jpg' in obj.key:
+    # list all keys in the input folder and only process those that
+    # have no associated events.xml file
+    for obj in bucket.objects.filter(Prefix=args.in_folder + '/'):
+        LOG.info('{0}:{1}'.format(args.in_folder, obj.key))
+        key = obj.key + '.events.xml'
+        if '.jpg' in obj.key and not file_exists(bucket_name, key):
             if channel.basic_publish('', q_name, obj.key):
                 LOG.info('Message has been delivered')
             else:
