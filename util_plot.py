a0d218d6ce3b0704b6b40d93ccde7e441dd752c6
Danelle Cline <dcline@mbari.org>
2016-11-09 22:33:19 -0800

    Added better heatmap and larger plots and some reformatting of the legend

--- util_plot.py
+++ util_plot.py
@@ -20,7 +20,7 @@ matplotlib.use('Agg')
 import json
 import io
 import os
-
+import math
 import numpy as np
 import tensorflow as tf
 import seaborn as sns
@@ -49,7 +49,7 @@ styles = markers + [
 
 colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
 
-def plot_confusion_matrix(args, classifier, bottlenecks, test_ground_truth, labels_list_file):
+def plot_confusion_matrix(args, classifier, bottlenecks, test_ground_truth, labels_list_file, labels_list_file_lt20):
   if gfile.Exists(labels_list_file):
         with open(labels_list_file, 'r') as lfile:
           labels_string = lfile.read()
@@ -57,6 +57,15 @@ def plot_confusion_matrix(args, classifier, bottlenecks, test_ground_truth, labe
           print("labels list: %s" % labels_list)
   else:
     print("Labels list %s not found" % labels_list_file)
+    return None
+
+  if gfile.Exists(labels_list_file_lt20):
+        with open(labels_list_file_lt20, 'r') as lfile:
+          labels_string = lfile.read()
+          labels_list_lt20 = json.loads(labels_string)
+          print("labels list: %s" % labels_list_lt20)
+  else:
+    print("Labels list %s not found" % labels_list_file_lt20)
     return None
 
   sess = tf.Session()
@@ -93,81 +102,87 @@ def plot_confusion_matrix(args, classifier, bottlenecks, test_ground_truth, labe
       matrix[predicted, actual] += 1
       j += 1
 
-    max_per_plot = 15
-    if len(labels_list) > max_per_plot:
-      num_plots = int(len(labels_list)/max_per_plot)
+    max_per_plot = 75
+    if len(labels_list) >= max_per_plot:
+      num_plots = int(math.ceil(float(len(labels_list))/float(max_per_plot)))
     else:
       num_plots = 1
 
-    with sns.color_palette("husl", 100):
-      fig = plt.figure(figsize=(11, 17));
-      sns.set()
-      writer = tf.train.SummaryWriter(args.model_dir)
-      gs = gridspec.GridSpec(num_plots, 1)
-      # compute Precision-Recall per each class and plot curve per each class
-      print('============>Creating ROC and CM Plots')
-      a = 0
-      for count, name in enumerate(labels_list, 1):
-          if not results_y_test[name] and not results_y_score[name]:
-            print('===========> Warning - no values assigned to %s <=======' % name)
-            results_y_test[name].append(0)
-            results_y_score[name].append(0)
+    fig = plt.figure(figsize=(11, 17));
+    sns.set()
+    writer = tf.train.SummaryWriter(args.model_dir)
+    gs = gridspec.GridSpec(num_plots, 1)
+    # compute Precision-Recall per each class and plot curve per each class
+    print('============>Creating ROC and CM Plots')
+    a = 0
+    j = 0
+    for count, name in enumerate(labels_list, 1):
+      color = colors[count % len(colors)]
+      style = linestyles[count % len(linestyles)]
+      marker = markers[count % len(markers)]
 
-          precision[name], recall[name], _ = precision_recall_curve(results_y_test[name], results_y_score[name])
+      # if one of the classes with less than 20 images, enlarge the marker a little
+      if name in labels_list_lt20:
+        marker_size = 10
+      else:
+        marker_size = 5
 
-          color = colors[count % len(colors)]
-          style = linestyles[count % len(linestyles)]
-          marker = markers[count % len(markers)]
-          if (count + max_per_plot) % max_per_plot  == 1:
-            ax = fig.add_subplot(gs[a])
-            ax.set_title('ROC Curves')
-            a += 1
-          if count % max_per_plot  == 0:
-            ax.legend(loc="lower left")
+      if (count + max_per_plot) % max_per_plot  == 1:
+        print('Adding subplot %d numplots %d %d' % (a, num_plots, len(labels_list)))
+        ax = fig.add_subplot(gs[a])
+        ax.set_title('Precision/Recall Curves ' + args.model_dir)
+        ax.set_xlim([0, 1.05])
+        ax.set_ylim([0, 1.05])
+        a += 1
+      if count % max_per_plot  == 0 or count == len(labels_list):
+        ax.legend(loc="lower left")
 
-          ax.plot(recall[name], precision[name], linestyle=style,  marker=marker, color=color, markersize=5,
-             label='Precision-recall curve of class {0}'
-                   ''.format(name));
+      if not results_y_test[name] and not results_y_score[name]:
+        print('===========> Warning - no values assigned to %s <=======' % name)
+        ax.annotate('unassigned category ' + name, xy=(1, 0 + j), xycoords='axes fraction', horizontalalignment='right',
+            verticalalignment='bottom', fontsize=10)
+        j += .02
+      else:
+        precision[name], recall[name], _ = precision_recall_curve(results_y_test[name], results_y_score[name])
+        ax.plot(recall[name], precision[name], linestyle=style,  marker=marker, color=color, markersize=marker_size,
+         label='category {0}'
+               ''.format(name));
 
-      '''avg_f_score = np.average(results.f_score)
-      avg_precision = np.average(results.precision)
-      avg_accuracy = np.average(results.accuracy)
-      annotation = '(averages) f_score:{0:0.2f} precision:{1:0.2f} accuracy:{2:0.2f}'\
-                      .format(avg_f_score, avg_precision, avg_accuracy)
-      ax2.annotate(annotation, xy=(1, 0), xycoords='axes fraction', horizontalalignment='right',
-                  verticalalignment='bottom', fontsize=10)
-      ax.legend(loc="lower left") '''
+    plt.savefig(os.path.join(args.model_dir, 'm_roc_' + args.metrics_plot_name), format='png', dpi=120);
+    buf2 = io.BytesIO()
+    plt.savefig(buf2, format='png', dpi=120);
+    buf2.seek(0)
+    #plt.show()
+    # Convert PNG buffer to TF image, add batch dimension and image summary; this will post the plot as an  image to tensorboard
+    image2 = tf.image.decode_png(buf2.getvalue(), channels=4)
+    image2 = tf.expand_dims(image2, 0)
+    head, tail = os.path.split(args.model_dir)
+    tag = '{0}/m_roc_{1}'.format(tail,args.metrics_plot_name)
+    summary_op = tf.image_summary(tag, image2)
+    summary = sess.run(summary_op)
+    writer.add_summary(summary)
 
-      plt.savefig(os.path.join(args.model_dir, 'm_roc_' + args.metrics_plot_name), format='png', dpi=120);
-      buf2 = io.BytesIO()
-      plt.savefig(buf2, format='png', dpi=120);
-      buf2.seek(0)
-      #plt.show()
-      # Convert PNG buffer to TF image, add batch dimension and image summary; this will post the plot as an  image to tensorboard
-      image2 = tf.image.decode_png(buf2.getvalue(), channels=4)
-      image2 = tf.expand_dims(image2, 0)
-      head, tail = os.path.split(args.model_dir)
-      tag = '{0}/m_roc_{1}'.format(tail,args.metrics_plot_name)
-      summary_op = tf.image_summary(tag, image2)
-      summary = sess.run(summary_op)
-      writer.add_summary(summary)
+    plt.close('all')
 
-      plt.close('all')
+    fig, ax = plt.subplots(figsize=(11, 11));
+    ax.set_title('Confusion Matrix ' + args.model_dir)
 
-      fig, ax = plt.subplots(figsize=(5, 5));
-      ax.set_title('Confusion Matrix ')
-      # compute confusion matrix and color with heatmap
-      sns.heatmap(matrix, ax=ax, annot=True, fmt='d', linewidths=.5, yticklabels=labels_list, xticklabels=labels_list)
-      plt.savefig(os.path.join(args.model_dir, 'm_cm_' + args.metrics_plot_name), format='png', dpi=120);
-      # Convert PNG buffer to TF image, add batch dimension and image summary; this will post the plot as an  image to tensorboard
-      buf3 = io.BytesIO()
-      plt.savefig(buf3, format='png', dpi=120);
-      buf3.seek(0)
-      image3 = tf.image.decode_png(buf3.getvalue(), channels=4)
-      image3 = tf.expand_dims(image3, 0)
-      head, tail = os.path.split(args.model_dir)
-      tag = '{0}/m_cm_{1}'.format(tail, args.metrics_plot_name)
-      summary_op3 = tf.image_summary(tag, image3)
-      summary3 = sess.run(summary_op3)
-      writer.add_summary(summary3)
-      writer.close()
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    # compute confusion matrix and color with heatmap
+    sns.heatmap(matrix, cmap=cmap, vmax=30, annot=False,
+          square=True, xticklabels=labels_list, yticklabels=labels_list,
+          linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
+    plt.savefig(os.path.join(args.model_dir, 'm_cm_' + args.metrics_plot_name), format='png', dpi=120);
+
+    # Convert PNG buffer to TF image, add batch dimension and image summary; this will post the plot as an  image to tensorboard
+    buf3 = io.BytesIO()
+    plt.savefig(buf3, format='png', dpi=120);
+    buf3.seek(0)
+    image3 = tf.image.decode_png(buf3.getvalue(), channels=4)
+    image3 = tf.expand_dims(image3, 0)
+    head, tail = os.path.split(args.model_dir)
+    tag = '{0}/m_cm_{1}'.format(tail, args.metrics_plot_name)
+    summary_op3 = tf.image_summary(tag, image3)
+    summary3 = sess.run(summary_op3)
+    writer.add_summary(summary3)
+    writer.close()
