34db44f385188ee041b264085c1823133d66ffc4
Danelle Cline <dcline@mbari.org>
2016-11-15 16:42:28 -0800

    Added batch run python to simplify testing; added resized image in preparation for further documentation

---
+++ batch_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run with nohup ./batch_run.sh > /tmp/nohup.out 2>&1
+source /home/dcline-admin/Dropbox/GitHub/venv-mbari-aesa-dev-box/bin/activate
+annotation_file=`pwd`/M56_Annotations_v10.csv
+#python=~/anaconda/bin/python
+python=python
+python batch_run.py
