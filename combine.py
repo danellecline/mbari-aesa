220118ef9619d98aeb2666621e830bda790c04ce
Danelle Cline <dcline@mbari.org>
2017-02-10 13:39:10 -0800

    utility to combine all dives

---
+++ combine.py
@@ -0,0 +1,38 @@
+import os
+import util
+import fnmatch
+from shutil import copyfile
+
+prefix_dirs = ['JC062_75pad', 'M535455_75pad', 'M56_75pad']
+dest_dir = os.path.join(os.getcwd(),'data', 'JC062_M535455_M56_75pad')
+util.ensure_dir(dest_dir)
+
+for prefix in prefix_dirs:
+
+  # image directory where cropped images are located
+  image_dir = os.path.join(os.getcwd(),'data', prefix,'images_category','cropped_images')
+
+  for root, dirnames, filenames in os.walk(image_dir):
+    root_bneck = root.replace('cropped_images', 'bottleneck')
+
+    for filename in fnmatch.filter(filenames, '*.jpg'):
+      # prepend the file with the prefix; copy both the image and bottleneck file if available
+      class_dirname = root.split('/')[-1]
+
+      fname_src = os.path.join(root, filename)
+      d = os.path.join(dest_dir, 'cropped_dir', class_dirname)
+      util.ensure_dir(d)
+      fname_dst = os.path.join(d, prefix + '_' + filename)
+      copyfile(fname_src, fname_dst)
+
+
+      fname_src = os.path.join(root_bneck, filename + '.txt')
+      d = os.path.join(dest_dir, 'bottleneck', class_dirname)
+      util.ensure_dir(d)
+      fname_dst = os.path.join(d, prefix + '_' + filename + '.txt')
+      if os.path.exists(fname_src):
+        copyfile(fname_src, fname_dst)
+
+print 'Done'
+
+
